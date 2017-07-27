3508b907519ab9be91ff7c26a8e30372b941272e
vanitanaidu <vanita112003@gmail.com>
2017-07-27 13:46:18 -0400

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var cake = {
     var status = "Decorating with " + this.topping + ". Ready to eat soon!"
     updateFunction(status)
     setTimeout(() => {
-      updateFunction(serve.apply(this, "Happy Eating!", this.customer))
+      updateFunction(serve.apply(this, ["Happy Eating!", this.customer]))
     }, 2000)
   }
 }
@@ -24,14 +24,17 @@ var pie = {
 }
 
 function makeCake() {
-  var updateCakeStatus = this.getElementsById("cake")[0].innerText
-  mix(updateCakeStatus)
+  var updateCakeStatus = updateStatus.bind(this)
+  updateCakeStatus("I am making a cake")
+  mix.call(cake, updateCakeStatus)
 }
 
 function makePie() {
-  var updatePieStatus = this.getElementsById("pie")[0].innerText
-  mix(updatePieStatus)
+  var updatePieStatus = updateStatus.bind(this)
+  updatePieStatus("I am making a pie")
   pie.decorate = cake.decorate.bind(pie)
+  mix.call(pie, updatePieStatus)
+
 }
 
 function updateStatus(statusText) {
@@ -40,29 +43,34 @@ function updateStatus(statusText) {
 
 function bake(updateFunction) {
   var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime
-  setTimeout(function() {
-    cool(updateFunction)
+  setTimeout(() => {
+    cool.call(this, updateFunction)
   }, 2000)
+  updateFunction(status)
 }
 
 function mix(updateFunction) {
   var status = "Mixing " + this.ingredients.join(", ")
-  setTimeout(function() {
-    bake(updateFunction)
+  setTimeout(() => {
+    bake.call(this, updateFunction)
   }, 2000)
   updateFunction(status)
 }
 
 function cool(updateFunction) {
   var status = "It has to cool! Hands off!"
-  setTimeout(function() {
+  setTimeout(() => {
     this.decorate(updateFunction)
   }, 2000)
+  updateFunction(status)
 }
 
 function makeDessert() {
-  //add code here to decide which make... function to call
-  //based on which link was clicked
+  if (this.parentNode.id ===  "cake") {
+    makeCake.call(this.parentNode)
+  } else {
+    makePie.call(this.parentNode)
+  }
 }
 
 function serve(message, customer) {
