c4e9e273971b29f31445f05bc14914d96c613964
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-08-10 16:21:20 -0400

    Done.

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var cake = {
   customer: "Tommy",
   decorate: function(updateFunction) {
     var status = "Decorating with " + this.topping + ". Ready to eat soon!"
-    updateFunction(status)
-    setTimeout(function() {
-      updateFunction(serve.apply(this, "Happy Eating!", this.customer))
+    updateFunction.apply(this, [status])
+    setTimeout(() => {
+      updateFunction(serve.apply(this, ["Happy Eating!", this.customer]))
     }, 2000)
   }
 }
