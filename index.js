e85d589038f43e8d4a965eed11101586aefcd55e
Luisa Scavo <luisascavo@gmail.com>
2017-08-26 01:07:28 +0000

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var cake = {
   decorate: function(updateFunction) {
     var status = "Decorating with " + this.topping + ". Ready to eat soon!"
     updateFunction(status)
-    setTimeout(function() {
-      updateFunction(serve.apply(this, "Happy Eating!", this.customer))
+    setTimeout(() => {
+      updateFunction(serve.call(this, "Happy Eating!", this.customer))
     }, 2000)
   }
 }
@@ -24,13 +24,14 @@ var pie = {
 }
 
 function makeCake() {
-  var updateCakeStatus;
-  mix(updateCakeStatus)
+  var updateCakeStatus = updateStatus.bind(this);
+  mix.call(cake, updateCakeStatus);
 }
 
 function makePie() {
-  var updatePieStatus;
-  mix(updatePieStatus)
+  var updatePieStatus = updateStatus.bind(this);
+  pie.decorate = cake.decorate.bind(pie);
+  mix.call(pie, updatePieStatus);
 }
 
 function updateStatus(statusText) {
@@ -39,14 +40,15 @@ function updateStatus(statusText) {
 
 function bake(updateFunction) {
   var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime
-  setTimeout(function() {
+  setTimeout(() => {
     cool(updateFunction)
   }, 2000)
+  updateFunction(status)
 }
 
 function mix(updateFunction) {
   var status = "Mixing " + this.ingredients.join(", ")
-  setTimeout(function() {
+  setTimeout(() => {
     bake(updateFunction)
   }, 2000)
   updateFunction(status)
@@ -54,14 +56,23 @@ function mix(updateFunction) {
 
 function cool(updateFunction) {
   var status = "It has to cool! Hands off!"
-  setTimeout(function() {
+  setTimeout(() => {
     this.decorate(updateFunction)
   }, 2000)
+  updateFunction(status)
 }
 
 function makeDessert() {
   //add code here to decide which make... function to call
   //based on which link was clicked
+  switch(this.innerHTML) {
+    case "Make Cake":
+      makeCake.call(this.parentNode);
+      break;
+    case "Make Pie":
+      makePie.call(this.parentNode);
+      break;
+  }
 }
 
 function serve(message, customer) {
